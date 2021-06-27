5e990e2a2ac1b820ee495934dc628f90862d71b1
DobbCraft <80113661+DobbCrafte@users.noreply.github.com>
2021-06-27 13:37:10 +0200

    added form

---
+++ chat.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="de">
+<link rel="stylesheet" href="style.css">
+<head>
+    <title>Chat</title>
+</head>
+<body>
+Welcome <?php
+echo $_GET["name"];
+?>
+
+
+</body>
+</html>
